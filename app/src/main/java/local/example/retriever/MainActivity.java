0c502be379847ca047968dafc5453a733dddff27
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-15 07:46:39 +0100

    Update MainActivity.java

--- app/src/main/java/local/example/retriever/MainActivity.java
+++ app/src/main/java/local/example/retriever/MainActivity.java
@@ -18,8 +18,9 @@
 
 package local.example.retriever;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
