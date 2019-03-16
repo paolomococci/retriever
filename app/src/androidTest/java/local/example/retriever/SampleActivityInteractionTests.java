605764e24282ae142222187112787dc13309fb5b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-16 10:27:16 +0100

    Create SampleActivityInteractionTests.java

---
+++ app/src/androidTest/java/local/example/retriever/SampleActivityInteractionTests.java
@@ -0,0 +1,46 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.retriever;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class SampleActivityInteractionTests {
+
+    public static final String TO_BE_TYPED = "I would like an truly italian espresso.";
+
+    @Rule
+    public ActivityScenarioRule<MainActivity>
+            activityScenario = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void interactionTypedTests() {
+        onView(withId(R.id.editText)).perform(typeText(TO_BE_TYPED));
+    }
+}
